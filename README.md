a7f7d04c3810b0a2bcf04a62a79ec7c1dbd13b54
Nguyễn Lê Anh Minh <anhminh2122000@gmail.com>
2020-03-16 23:54:13 +0700

    Website ver2.0/beta

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# https://hugoclub.github.io
+# Hugo English Club
+> HUGO ENGLISH CLUB - VERSION 2.0
+
+Đây là hệ thống website thông tin của Câu Lạc Bộ Hugo.
+
+* Hệ thống nội quy mới về HĐ Câu lạc bộ
+* Hệ thống Hoạt động Online trên Hugo Group
+* Website thông tin CLB Hugo
+
+_Các thay đổi được áp dụng từ ngày 16/3/2020._
+
+https://hugoclub.github.io
---
+++ precache-manifest.fa4061f8a1b3c77f99e199fdd4f24c74.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6247f0b8400cfb562fa605531a2c9ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b47fa58a88fe6bf1cd3a",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c6b7d949495fba8676aa",
+    "url": "/static/css/main.2d1e93a0.chunk.css"
+  },
+  {
+    "revision": "b47fa58a88fe6bf1cd3a",
+    "url": "/static/js/2.01e337d4.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.01e337d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6b7d949495fba8676aa",
+    "url": "/static/js/main.a4810226.chunk.js"
+  },
+  {
+    "revision": "4ed8df92525e17632295",
+    "url": "/static/js/runtime-main.dc1d1cc1.js"
+  },
+  {
+    "revision": "0027fe8f886a60d499a5b46db4a67fb6",
+    "url": "/static/media/logo.0027fe8f.png"
+  }
+]);
